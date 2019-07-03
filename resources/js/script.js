6ce06e1a3d6ae712b3cc2e70cbfb69eda38373cf
Marc <marc.manillier@live.fr>
2019-07-03 11:51:17 -0400

    Création de compte, infos de base fonctionnelles

---
+++ resources/js/script.js
@@ -0,0 +1,27 @@
+$(() => {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// FONCTION NE FONCTIONNE PAS, AJOUT TEMPORAIRE AU BAS DU FICHIER APP.BLADE.PHP
+	// Afficher la valeur du range max_distance form register
+	// function updateMaxDistanceValue(val) {
+	// 	document.getElementById('max_distance_value').value=val
+	// }
+
+})
