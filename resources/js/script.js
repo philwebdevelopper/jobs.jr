75eb459443512df5211f4deb27b17b8ba888e502
Marc <marc.manillier@live.fr>
2019-07-03 12:04:31 -0400

    conflict resolve

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,11 +1,27 @@
 $(() => {
 
-    // Afficher la valeur du range max_distance form register
-	function updateTextInput(val) {
-		document.getElementById('max_distance_value').value=val; 
-	}
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// FONCTION NE FONCTIONNE PAS, AJOUT TEMPORAIRE AU BAS DU FICHIER APP.BLADE.PHP
+	// Afficher la valeur du range max_distance form register
+	// function updateMaxDistanceValue(val) {
+	// 	document.getElementById('max_distance_value').value=val
+	// }
 
 })
