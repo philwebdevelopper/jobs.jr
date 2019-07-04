91e4a5091a2ca7dedaca420f42bb7e69c2269aa7
Marc <marc.manillier@live.fr>
2019-07-04 12:06:42 -0400

    ajout champs adresse formulaire inscription

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,7 +10,7 @@
 				<div class="card-body">
 					<form method="POST" action="{{ route('register') }}">
 						@csrf
-						
+
 						{{-- Nom --}}
 						<div class="form-group row">
 							<label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nom') }}</label>
@@ -73,6 +73,69 @@
 								<input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email" required>
 
 								@error('email')
+									<span class="invalid-feedback" role="alert">
+										<strong>{{ $message }}</strong>
+									</span>
+								@enderror
+							</div>
+						</div>
+
+						{{-- Adresse --}}
+
+						{{-- Rue --}}
+						<div class="form-group row">
+							<label for="street" class="col-md-4 col-form-label text-md-right">{{ __('Adresse') }}</label>
+
+							<div class="col-md-6">
+								<input id="street" type="text" class="form-control @error('street') is-invalid @enderror" name="street" value="{{ old('street') }}" required autocomplete="street" autofocus>
+
+								@error('street')
+									<span class="invalid-feedback" role="alert">
+										<strong>{{ $message }}</strong>
+									</span>
+								@enderror
+							</div>
+						</div>
+
+						{{-- Code postal --}}
+						<div class="form-group row">
+							<label for="zip_code" class="col-md-4 col-form-label text-md-right">{{ __('Code postal') }}</label>
+
+							<div class="col-md-6">
+								<input id="zip_code" type="text" class="form-control @error('zip_code') is-invalid @enderror" name="zip_code" value="{{ old('zip_code') }}" required autocomplete="zip_code" autofocus>
+
+								@error('zip_code')
+									<span class="invalid-feedback" role="alert">
+										<strong>{{ $message }}</strong>
+									</span>
+								@enderror
+							</div>
+						</div>
+
+
+						{{-- Ville --}}
+						<div class="form-group row">
+							<label for="city" class="col-md-4 col-form-label text-md-right">{{ __('Ville') }}</label>
+							
+							<div class="col-md-6">
+								<input id="city" type="text" class="form-control @error('city') is-invalid @enderror" name="city" value="{{ old('city') }}" required autocomplete="city" autofocus>
+
+								@error('city')
+									<span class="invalid-feedback" role="alert">
+										<strong>{{ $message }}</strong>
+									</span>
+								@enderror
+							</div>
+						</div>
+
+						{{-- Numéro appartement --}}
+						<div class="form-group row">
+							<label for="apartement" class="col-md-4 col-form-label text-md-right">{{ __('No. App.') }}</label>
+							
+							<div class="col-md-6">
+								<input id="apartement" type="number" class="form-control @error('apartement') is-invalid @enderror" name="apartement" value="{{ old('apartement') }}" required autocomplete="apartement" autofocus>
+
+								@error('apartement')
 									<span class="invalid-feedback" role="alert">
 										<strong>{{ $message }}</strong>
 									</span>
