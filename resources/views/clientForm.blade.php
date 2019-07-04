6f5d33d1c75d449ceeafdfb8c59597c4eba64846
Autologon Usagers Multi <autologon_multi@cegep-fxg.qc.ca>
2019-07-04 14:02:19 -0400

    Création du form pour le client

---
+++ resources/views/clientForm.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <h1>Quel est votre demande ?</h1>
+  <form>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="email">Email</label>
+      <input type="email" class="form-control" id="email" placeholder="email@email.com">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="phone">Phone Number</label>
+      <input type="tel" class="form-control" id="phone" placeholder="(123)-456-7890">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputAddress">Address</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+  </div>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="city">City</label>
+      <input type="text" class="form-control" id="city">
+    </div>
+    <div class="form-group col-md-2">
+      <label for="zipCode">Zip Code</label>
+      <input type="text" class="form-control" id="zipCode">
+    </div>
+  </div>
+  <div class="form-row">
+    <div class="form-group">
+      <label for="typeService">Type de service</label>
+      <select id="typeService" class="form-control">
+        <option selected>Choose...</option>
+        <option>Peinture</option>
+        <option>Déneigement</option>
+        <option>Gardiennage</option>
+        <option>Entretien Paysager</option>
+      </select>
+    </div>
+    <div class="form-group col-md-6">
+      <label for="dateService">Date du service</label>
+      <input type="date" class="form-control" id="dateService">
+    </div>
+</div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/clientForm', function (){
+  return view('clientForm');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
