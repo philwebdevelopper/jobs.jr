fac67891945ec7e092470d067ac8220f405b6151
Marc <marc.manillier@live.fr>
2019-07-06 10:18:03 -0400

    Divers reglages de base (layout app + welcome + style)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,22 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Tableau de bord</div>
+	<div class="row justify-content-center">
+		<div class="col-md-8">
+			<div class="card">
+				<div class="card-header">Tableau de bord</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+				<div class="card-body">
+					@if (session('status'))
+					<div class="alert alert-success" role="alert">
+						{{ session('status') }}
+					</div>
+					@endif
 
-                    Vous êtes connecté!
-                </div>
-            </div>
-        </div>
-    </div>
+					Vous êtes connecté!
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 @endsection
--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -4,14 +4,14 @@
 @php
 if (isset($_POST['clientLat']) && isset($_POST['clientLon']))
 {
-  $lat = $_POST['clientLat'];
-  $lon = $_POST['clientLon'];
+	$lat = $_POST['clientLat'];
+	$lon = $_POST['clientLon'];
 }
 @endphp
 <div class="container">
-    <div id="mapid"></div>
-    <input type="hidden" id="lat" name="clientLat" value="<?php echo $lat ?>">
-    <input type="hidden" id="lon" name="clientLon" value="<?php echo $lon ?>">
+	<div id="mapid"></div>
+	<input type="hidden" id="lat" name="clientLat" value="<?php echo $lat ?>">
+	<input type="hidden" id="lon" name="clientLon" value="<?php echo $lon ?>">
 </div>
 @endsection
 
